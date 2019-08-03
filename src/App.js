0a668bf308850d6073d00096ec81be3aa4354948
MohammadAlderson <mohammadhooshmand76@gmail.com>
2019-08-03 11:01:53 +0430

    added last project stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   render() {
+      return (
+         <div className="main">
+            <h1 className="page-title">BS3 To BS4 Convertor</h1>
+            <div className="form-container">
+               <h3 className="input-title">BS3 Input</h3>
+               <div className="form-item">
+                  <label className="form-label">Col-Xs- :</label>
+                  <input
+                     id="Col-Xs"
+                     className="form-input"
+                     onchange="onChangeCol_XsValue()"
+                     type="text"
+                  />
+               </div>
+               <div className="form-item">
+                  <label className="form-label">Col-Sm- :</label>
+                  <input
+                     id="Col-Sm"
+                     className="form-input"
+                     onchange="onChangeCol_SmValue()"
+                     type="text"
+                  />
+               </div>
+               <div className="form-item">
+                  <label className="form-label">Col-Md- :</label>
+                  <input
+                     id="Col-Md"
+                     className="form-input"
+                     onchange="onChangeCol_MdValue()"
+                     type="text"
+                  />
+               </div>
+               <div className="form-item">
+                  <label className="form-label">Col-Lg- :</label>
+                  <input
+                     id="Col-Lg"
+                     className="form-input"
+                     onchange="onChangeCol_LgValue()"
+                     type="text"
+                  />
+               </div>
+               <div className="form-item btns-container">
+                  <button
+                     className="form-btn"
+                     id="convertorBtn"
+                     onclick="btnClick()"
+                  >
+                     Convert
+                  </button>
+                  <button
+                     className="form-btn"
+                     id="clearBtn"
+                     onclick="btnClear()"
+                  >
+                     Clear
+                  </button>
+               </div>
+            </div>
+            <div className="output-box">
+               <h3 className="output-title">BS4 Output</h3>
+               <h2 className="output-text" id="output">
+                  Waiting...
+               </h2>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default App;
